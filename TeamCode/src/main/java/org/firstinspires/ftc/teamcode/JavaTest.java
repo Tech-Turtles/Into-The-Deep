f8d64d610ed5e29512eb4bac06c937cff0e00b0a
Christian Esneul <nawaplayz@gmail.com>
2024-11-11 18:54:11 -0600

    feed forward and top bucket preset angle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JavaTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JavaTest.java
@@ -5,12 +5,49 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "Cheeseburger")
 public class JavaTest extends OpMode {
+    boolean intDone;
+    int x;
+    int y;
+
+    public JavaTest(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString(){
+        return "JavaTest " + x + " " + y;
+    }
+
     @Override
     public void init() {
+        telemetry.addData("int Done", intDone);
+        intDone = true;
+    }
+    //returnDataType name(parameters)
+    double squareInputWithSign(double input){
+        double output = input * input;
+        if (input < 0) {
+            output = output * -1;
+        }
+        return output;
     }
 
     @Override
     public void loop() {
-        double speedForward = -gamepad1.left_stick_y / 2.0;
+        double leftAmount = gamepad1.left_stick_x;
+        double fwdAmount = -gamepad1.left_stick_y;
+
+        telemetry.addData("Before X", leftAmount);
+        telemetry.addData("Before Y", fwdAmount);
+
+        leftAmount = squareInputWithSign(leftAmount);
+        fwdAmount = squareInputWithSign(fwdAmount);
+
+        telemetry.addData("After X", leftAmount);
+        telemetry.addData("After Y", fwdAmount);
+
+        telemetry.addData("int Done", intDone);
+
     }
 }
